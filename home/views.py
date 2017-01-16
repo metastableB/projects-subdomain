df6dfa0b1a3b54beb1b75e2ea8db93c000967c3a
metastableB <donkdennis@gmail.com>
2017-01-16 17:52:26 +0530

    Include extend structure and simple home page

--- home/views.py
+++ home/views.py
@@ -2,8 +2,8 @@
 # @author:Don Dennis
 # views.py
 #
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'home/index.html')
